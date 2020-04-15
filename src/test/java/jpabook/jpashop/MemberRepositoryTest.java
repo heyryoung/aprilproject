4e3890c4cb3e0865fd00f70662d233e3c8c5c755
Kwon Hye-ryung <leja0418@gmail.com>
2020-04-15 20:43:25 +0900

    first Commit

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -34,7 +34,7 @@ public class MemberRepositoryTest {
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         Assertions.assertThat(findMember).isEqualTo(member);
 
-    
+
     }
     
 }
